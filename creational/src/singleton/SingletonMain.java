88bfc0f775a0b32f2e884d7b9a01a77aa799baa7
wuchong <wuchong@Streamax.com>
2020-04-16 15:10:38 +0800

    单例模式

---
+++ creational/src/singleton/SingletonMain.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public class SingletonMain {
+    public static void main(String[] args) {
+
+        //不合法的构造函数
+        //编译时错误：构造函数 SingleObject() 是不可见的
+        //SingleObject object = new SingleObject();
+
+        //获取唯一可用的对象
+        SingleObject single = SingleObject.getInstance();
+
+        //显示消息
+        single.showMessage();
+    }
+}
